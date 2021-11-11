895e6da12aecaeb836f21477c63bd1afa80b18cd
giftilicious <90150892+giftilicious@users.noreply.github.com>
2021-11-10 23:58:54 -0500

    Added PUT and POST routes

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -15,8 +15,35 @@ router.get('/', async (req, res) => {
 });
 
 // router.put
+router.put('/:id', async (req, res) => {
+  try {
+      const workoutData = await Workout.update(req.body, {
+      where: {
+          id: req.params.id,
+      },
+      });
+      // if (!workoutData[0]) {
+      // res.status(404).json({ message: 'No indoor activity with this ID' });
+      // }
+      res.status(200).json(workoutData);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+  });
 
 // router.post
+router.post('/', async (req, res) => {
+  try {
+    const workoutData = await Workout.create(
+      req.body
+    );
+    res.status(200).json(workoutData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // router.get /api/workouts/range
 
+
+module.exports = router
