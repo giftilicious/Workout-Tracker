9af73d4870ef6a26c5eea5ccd4eb0bdf843eeb3d
giftilicious <90150892+giftilicious@users.noreply.github.com>
2021-11-11 22:22:48 -0500

    Modified seed file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutdb', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 const workoutSeed = [
   {
